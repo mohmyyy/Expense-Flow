b16096fa94da6eeadf647eb92a1f446a695ccd8e
mohmyyy <iammohmy@gmail.com>
2023-03-18 16:11:23 +0530

    adding Verify User Email authentication

--- src/Pages/ProfilePage.js
+++ src/Pages/ProfilePage.js
@@ -1,6 +1,26 @@
+import { useContext } from "react";
+import { AuthContext } from "../components/store/auth-context";
 import CompleteProfile from "./CompleteProfile";
 import classes from "./ProfilePage.module.css";
 const ProfilePage = () => {
+  const ctx = useContext(AuthContext);
+  const verifyUserHandler = async () => {
+    const response = await fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyDqf51p2j8MCmXzGVzjWDTqPIRvyMr5KUE",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          requestType: "VERIFY_EMAIL",
+          idToken: ctx.token,
+        }),
+        headers: {
+          "Content-type": "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+    console.log(data);
+  };
   return (
     <div>
       <div className={classes.header}>
@@ -11,6 +31,10 @@ const ProfilePage = () => {
             <a href="/complete-profile">Complete Now</a>
           </span>
         </h2>
+      </div>
+      <div className={classes["verify-item"]}>
+        <h3>Click here to verify your email</h3>
+        <button onClick={verifyUserHandler}>Verify Email</button>
       </div>
     </div>
   );
